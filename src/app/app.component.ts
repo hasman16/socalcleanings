817bae38937bb89a4299c89c5b6d22c947bff780
Ahmadou Ndoungm <hasman16@gmail.com>
2018-07-11 21:28:20 -0700

    First commit to GitHub

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Welcome to {{name}}</h1><span style="color:red;font-size:46px;font-weight:bolder">Site under construction. <br><br><br>Please check back later.</span>`,
+  template: `<h1>Welcome to {{name}}</h1><span style="color:red;font-size:46px;font-weight:bolder">Site under construction. <br><br><br>Please check back later.</span><br><br><img src="./app/images/cleaning.gif" class="img-fluid img-responsive" width="auto" alt="Clean Clean Clean">`,
 })
 export class AppComponent  { name = 'SoCal Cleanings'; }
