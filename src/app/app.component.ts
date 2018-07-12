42b7ad5ea5e24554737ac4298d1638eb74d0c259
Ahmadou Ndoungm <hasman16@gmail.com>
2018-07-11 18:06:37 -0700

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `<h1>Welcome to {{name}}</h1><span style="color:red;font-size:46px;font-weight:bolder">Site under construction. <br><br><br>Please check back later.</span>`,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  { name = 'SoCal Cleanings'; }
